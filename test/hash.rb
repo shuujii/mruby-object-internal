0ffc7929153607b335e468e8a08be9df8f9e9f99
KOBAYASHI Shuji <shuujii@gmail.com>
2020-05-12 17:07:25 +0900

    Small change

--- test/hash.rb
+++ test/hash.rb
@@ -2,6 +2,15 @@
 # Hash Tests (strongly dependent on the implementation)
 #
 
+#
+# TODO:
+#   EA に重複キーがあるときに IB が拡張すると、size/n_used が EA と IB で一
+#   致しなくなる。これを一致するようにすることは (ht_set 相当の処理を行えば)
+#   可能だとは思うが遅くなりそう。IB の size/n_used のほうが小さい場合は動作
+#   的には問題ない気がするのと、rehash 以外ではこの状態が解消されないのはお
+#   かしくない気もするので、そのままで良さそう。
+#
+
 class HashKey
   attr_accessor :value, :error, :callback
 
@@ -57,7 +66,7 @@ def ar_entries
   HashEntries[
     [1, "one"],
     [HashKey[2], :two],
-    [nil, 1],
+    [nil, :two],
     [:one, 1],
     ["NIL", nil],
     [HashKey[6], :six],  # same hash code as HashKey[2]
@@ -70,7 +79,7 @@ def ht_entries
     ["id", 32],
     [:date, "2020-05-02"],
     [200, "OK"],
-    ["modifiers", ["left_shift"]],
+    ["modifiers", ["left_shift", "control"]],
     ["key_code", "h"],
     ["h", 0x04],
     [[3, 2, 1], "three, two, one"],
@@ -84,8 +93,8 @@ def assert_hash_internal(exp, act)
   assert 'hash internal' do
     exp.each do |k, v|
       next pass if v == act.__send__(k)
-      msg = "Expected the following value of '#{k}' to be #{v.inspect}.\n" \
-            "#{act.ii}"
+      msg = "Expected the following value of '#{k}' to be #{v.inspect}.\n" +
+            act.ii
       lines = []
       msg.each_line{|line| lines << "    #{line}"}
       flunk "", lines.join
@@ -101,12 +110,15 @@ end
 
 assert 'mrb_hash_new_capa()' do
   assert_new_capa [[:ar?, true], [:ea, nil]], 0
+  assert_new_capa [[:ar?, true]], 1
   assert_new_capa [[:ar?, true]], 7
   assert_new_capa [[:ar?, true]], 16
   assert_new_capa [[:ar?, false], [:ib_bit, 5]], 17
   assert_new_capa [[:ar?, false], [:ib_bit, 11]], 1536
   assert_new_capa [[:ar?, false], [:ib_bit, 12]], 1537
 end
+
+# TODO: mrb_hash_merge() など他の MRB_API のテストも必要
 
 assert 'Hash#[]= internal' do
   size = 0
@@ -161,3 +173,12 @@ assert 'Hash#clear internal' do
     ], h
   end
 end
+
+#assert 'Large Hash' do
+#  entries = (1..70000).map {|n| [n, n * 2]}
+#  h = {}
+#  entries.each {|k, v| h[k] = v}
+#  assert_equal(entries.size, h.size)
+#  assert_equal(entries, h.to_a)
+#  entries.each {|k, v| assert_equal(v, h[k])}
+#end
