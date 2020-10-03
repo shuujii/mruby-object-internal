734b282d65c009b1872d180de4f5f831e8ff9705
KOBAYASHI Shuji <shuujii@gmail.com>
2020-10-03 21:21:52 +0900

    Refactor `mrb_hash_new_capa()` test

--- test/hash.rb
+++ test/hash.rb
@@ -116,7 +116,7 @@ end
 def assert_hash_internal(exp, act)
   assert 'hash internal' do
     exp.each do |k, v|
-      next pass if v == act.__send__(k)
+      next pass if v === act.__send__(k)
       msg = "Expected the following value of '#{k}' to be #{v.inspect}.\n" +
             act.ii
       lines = []
@@ -127,24 +127,31 @@ def assert_hash_internal(exp, act)
   end
 end
 
-def assert_new_capa(exp, capa)
-  exp += [[:size, 0], [:ea_capacity, capa], [:ea_n_used, 0]]
-  assert_hash_internal(exp, HashTest.new_with_capacity(capa))
-end
-
 def assert_modified_error(&block)
   assert_raise_with_message(RuntimeError, "hash modified", &block)
 end
 
 assert 'mrb_hash_new_capa()' do
-  assert_new_capa [[:ar?, true], [:ea, nil]], 0
-  assert_new_capa [[:ar?, true]], 1
-  assert_new_capa [[:ar?, true]], 7
-  assert_new_capa [[:ar?, true]], 16
-  assert_new_capa [[:ar?, false], [:ib_bit, 5]], 17
-  assert_new_capa [[:ar?, false], [:ib_bit, 8]], 128
-  assert_new_capa [[:ar?, false], [:ib_bit, 11]], 1536
-  assert_new_capa [[:ar?, false], [:ib_bit, 12]], 1537
+  #  capa, [   ar,       ea, ib_bit]
+  [ [   0, [ true,      nil,    nil]],
+    [   1, [ true,  Numeric,    nil]],
+    [   7, [ true,  Numeric,    nil]],
+    [  16, [ true,  Numeric,    nil]],
+    [  17, [false,  Numeric,      5]],
+    [ 128, [false,  Numeric,      8]],
+    [1535, [false,  Numeric,     11]],
+    [1537, [false,  Numeric,     12]],
+  ].each do |capa, (ar, ea, ib_bit)|
+    h = HashTest.new_with_capacity(capa)
+    assert_hash_internal [
+      [:ar?, ar],
+      [:size, 0],
+      [:ea, ea],
+      [:ea_capacity, capa],
+      [:ea_n_used, 0],
+      [:ib_bit, ib_bit],
+    ], h
+  end
 end
 
 assert 'mrb_hash_foreach()' do
@@ -555,7 +562,7 @@ assert 'EA and IB expansion at the same time' do
 end
 
 if RUN_SLOW_TEST
-  assert 'Large Hash' do
+  assert 'large Hash' do
     entries = (1..70000).map {|n| [n, n * 2]}
     h = {}
     entries.each {|k, v| h[k] = v}
